2ff5ee6871c2a819be7f715565fb9ca254502476
GenossenCoder <xperte9988@gmail.com>
2022-02-10 12:54:23 +0100

    working:running, deletPoll, getPoll, addVote, createPoll

--- Database.py
+++ Database.py
@@ -1,29 +1,62 @@
 import certifi
 from pymongo import MongoClient
 from datetime import datetime, timedelta
+from bson.objectid import ObjectId
 
 def Connect():
     connection_string = "mongodb+srv://adim:WknSllcgkPnBuuR1@cluster0.uw8iy.mongodb.net/Discord?retryWrites=true&w=majority"
     client  = MongoClient(connection_string, tlsCAFile=certifi.where())
-    db = client['Discord']#
+    db = client['Discord']
     Events = db.Event
     return Events
 
-def createPoll(text,title,pollType,competetors,deadline):
-    
+def createPoll(text,title,pollType,competetors,deadline):  
     Events=Connect();
-    votes=[]
     if pollType=="multi":
-        for i in competetors:
-           votes.append([])
         Event_single = {
         "title": title,
         "text": text,
         "competetors":competetors,
         "createdAt": datetime.utcnow(),
         "deadline":datetime.now() + timedelta(hours=deadline-1),
-        "votes": votes
-        }
-        
-    print(Events)
+        } 
+        #appending competetors to the dictonary
+        for i in range(len(competetors)):
+            Event_single[competetors[i]]=[]
     Events.insert_one(Event_single)
+
+def addVote(id,vote,name):
+    Events=Connect();
+    Events.update_one({"_id" :ObjectId(id)},{"$push":{vote:name}})
+    return("sucessfull")
+
+
+def getPoll(id):
+    List=[]
+    Events=Connect()
+    Poll=Events.find_one({"_id" :ObjectId(id)})
+    List.append(Poll['competetors'])
+    List.append([])
+    for i in Poll['competetors']:
+        List[1].append(Poll[i])
+    return List
+
+
+def deletePoll(id):
+    Events=Connect()
+    Events.delete_one({"_id" :ObjectId(id)})
+    print(str(id)+" deleted")
+    
+
+def running():
+    Events=Connect()
+    Polls=Events.find()
+    runningPolls=[]
+    for i in Polls:
+        if i['deadline'] > datetime.utcnow():
+            runningPolls.append(i['_id'])
+    return runningPolls
+#running()
+#print(getPoll("6204fa9bb6436cbdfc1c9e8d"))
+#addVote("6204fa9bb6436cbdfc1c9e8d","Jasmin","du")
+#createPoll("text","title","multi",["Zombie", "Jasmin","Olaf","Clemens"],9)S
